34d841677d2e5959e31077854818f0ef933d3411
MassimoAurelio <massimoaureliowork@gmail.com>
2024-01-17 16:32:01 +0300

    first

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+        <NuxtLink to="/" class="">
+            <NuxtImg src="/OpenAI.dark.svg" width="100px"  class="mx-auto"/>
+        </NuxtLink>
+
+        <UiButton>Send</UiButton>
+    </aside>
+</template>
+
+
+
---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Lato: {
+            wght: [300, 400, 700],
+            ital: [300],
+          },
+        },
+      },
+    ],
+  ],
+
+  shadcn: {
+    prefix: "Ui",
+    componentDir: "./components/ui",
+  },
+});
