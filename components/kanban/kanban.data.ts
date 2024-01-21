1d8f621b20df779f2fdf477fc93a720ff535689e
MassimoAurelio <massimoaureliowork@gmail.com>
2024-01-21 16:59:34 +0300

    update №2

---
+++ components/kanban/kanban.data.ts
@@ -0,0 +1,30 @@
+import { EnumStatus } from "~/types/deals.types";
+import type { IColumn } from "./kanban.types";
+
+export const KANBAN_DATA: IColumn[] = [
+  {
+    id: EnumStatus.todo,
+    name: "Входящие",
+    item: [],
+  },
+  {
+    id: EnumStatus["to-be-agreed"],
+    name: "На соглосовании",
+    item: [],
+  },
+  {
+    id: EnumStatus["in-progress"],
+    name: "В производстве",
+    item: [],
+  },
+  {
+    id: EnumStatus.produced,
+    name: "Произведено",
+    item: [],
+  },
+  {
+    id: EnumStatus.done,
+    name: "К отгрузке",
+    item: [],
+  },
+];
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import type { ICard, IColumn } from '~/components/kanban/kanban.types';
+import { useKanbanQuery } from '@/components/kanban/useKanbanQuery'
 
+useSeoMeta({
+    title: "Home | CRM System"
+})
+
+const dragCardRef = ref<ICard | null>(null)
+const sourceColumnRef = ref<IColumn | null>(null)
+
+const { data, isLoading, refetch } = useKanbanQuery()
 </script>
 
 <template>
     <div class="p-10">
         <h1 class="font-bolt text-2x1 mb-10">CRM System</h1>
+    </div>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else>
+        <UiCard class="mb-3 ml-5 mr-5" draggable="true">
+            <UiCardHeader role="button"> name card </UiCardHeader>
+            <UiCardContent> Компания </UiCardContent>
+            <UiCardFooter> Date </UiCardFooter>
+        </UiCard>
     </div>
 </template>
