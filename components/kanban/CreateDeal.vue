1aa58f11f5ca559f5c3e3d1b8ca0d5aaa03c281c
MassimoAurelio <massimoaureliowork@gmail.com>
2024-01-22 12:29:21 +0300

    update

---
+++ components/kanban/CreateDeal.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { useMutation } from '@tanstack/vue-query'
+import { v4 as uuid } from 'uuid'
+import { defineProps } from 'vue'
+import { COLLECTION_DEALS, DB_ID } from '@/app.constanst'
+import type { IDeal } from '~/types/deals.types'
+
+</script>
